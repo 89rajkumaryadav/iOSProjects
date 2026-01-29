41dde1da09b5339d66b2e86f8010c7309c583a6a
89rajkumaryadav <89rajkumaryadav@gmail.com>
2026-01-29 15:08:03 +0530

    Added MVC

---
+++ Design_Pattern/MVC/MVC_SwiftUI/MVC_SwiftUI/MVC_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVC_SwiftUIApp.swift
+//  MVC_SwiftUI
+//
+//  Created by Rajkumar Yadav on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MVC_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
